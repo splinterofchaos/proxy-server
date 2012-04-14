738a6a3c5d196d173562af6bc83a9e0f95e1b021
Scott Prager <hakusa@gmail.com>
2012-04-14 14:50:42 -0400

    Improved request handling.
    main forks when calling handle_client and returns right on to waiting for the
    next one. handle_client uses buffering to dynamically allocate messages.

--- proxy.cpp
+++ proxy.cpp
@@ -19,39 +19,34 @@ void die(char *errorMessage)
 }
 
 
-void handle_client(int clntSocket)
+struct Filefd
 {
-    const int BUF_SIZE = 256;
-    char echoBuffer[BUF_SIZE];
+    int fd;
 
-    /* Receive message from client */
-    if( recv(clntSocket, echoBuffer, RCVBUFSIZE, 0) < 0 )
-        die("recv() failed");
+    Filefd();
+    Filefd( int fd );
+    ~Filefd(); // Closes fd.
 
-    puts( echoBuffer );
+    operator int& (); // returns fd.
+};
 
-    std::string helloWorldHtml = 
-        std::string("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n") +
-        "<html> <head> <title>Tutorial: HelloWorld</title> </head> <body> <h1>HelloWorld Tutorial</h1> </body> </html>";
-    /* Echo message back to client */
-    if( send(clntSocket, helloWorldHtml.data(), helloWorldHtml.size(), 0) != helloWorldHtml.size() )
-        die("send() failed");
-
-
-    close(clntSocket);    /* Close client socket */
-}
+Filefd:: Filefd(        ) : fd(-1) {}
+Filefd:: Filefd( int fd ) : fd(fd) {}
+Filefd::~Filefd() { close( fd ); }
+Filefd::operator int& () { return fd; }
 
 struct PortListener
 {
-    int sock;
+    Filefd sock;
     sockaddr_in addr;
 
     PortListener( int port );
 };
 
 PortListener::PortListener( int port )
+    : sock( socket(PF_INET, SOCK_STREAM, IPPROTO_TCP) )
 {
-    if( (sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 )
+    if( sock < 0 )
         die( "socket(...) failed." );
 
     memset( &addr, 0, sizeof addr );
@@ -64,6 +59,27 @@ PortListener::PortListener( int port )
 
     if( listen(sock, 5) < 0 )
         die( "listen(...) failed." );
+}
+
+struct Responder
+{
+    Filefd sock;
+    sockaddr_in addr;
+
+    Responder();
+
+    int accept( int fd );
+};
+
+Responder::Responder()
+{
+}
+
+int Responder::accept( int fd )
+{
+    socklen_t s = sizeof addr;
+    sock = ::accept( fd, (sockaddr*)&addr, &s );
+    return sock;
 }
 
 int main(int argc, char *argv[])
@@ -85,15 +101,56 @@ int main(int argc, char *argv[])
         /* Set the size of the in-out parameter */
         clntLen = sizeof(echoClntAddr);
 
+        Responder resp;
         /* Wait for a client to connect */
         if ((clntSock = accept(listener.sock, (struct sockaddr *) &echoClntAddr, 
                                &clntLen)) < 0)
             die("accept() failed");
 
-        /* clntSock is connected to a client! */
-        printf( "Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr) );
+        switch( fork() )
+        {
+          case 0: close( listener.sock );
+                  handle_client( clntSock );
+                  return 0;
 
-        handle_client(clntSock);
+          case -1: die( "fork failed." );
+
+          default: close( clntSock );
+        }
     }
     /* NOT REACHED */
+}
+
+bool ends_with( const std::string& word, const char* const suffix )
+{
+    return word.rfind( suffix, word.size() - strlen(suffix) ) != std::string::npos;
+}
+
+void handle_client( int client )
+{
+    char buf[125];
+    std::string msg;
+    int n;
+
+    while( not ends_with(msg, "\r\n\r\n") 
+           && ( n = recv(client, buf, sizeof buf, 0) ) > 0 )
+        if( n > 0 )
+            msg.append( buf, n );
+
+    if( n < 0 )
+        die( "recv(...) failed." );
+
+    puts( msg.c_str() );
+
+    std::string helloWorldHtml = 
+        "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"
+        "<html>"
+            "<head> <title>Tutorial: HelloWorld</title> </head>"
+            "<body> <h1>HelloWorld Tutorial</h1>        </body>"
+        "</html>\r\n\r\n";
+    /* Echo message back to client */
+    if( send(client, helloWorldHtml.c_str(), helloWorldHtml.size(), 0) != helloWorldHtml.size() )
+        die("send() failed");
+
+    close( client );
 }
